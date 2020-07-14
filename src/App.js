1f57438fa5d360b9346fb6d5b1608d2f6aa3d601
Ted Bogin <tedbogin@Teds-MacBook-Pro.local>
2020-07-14 15:25:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import catImg from './cat.jpg';
+import dogImg from './dog.jpg';
 
-function App() {
+const App = () => {
+
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleChange = (e) => {
+    setSelectedOption(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Dog or Cat?</div>
+      <form className="pet-form">
+        <input 
+          type="radio" 
+          name="option-btn" 
+          value="cat" 
+          checked={selectedOption === 'cat'}
+          onChange={handleChange}/>
+        <label>Cat</label><br />
+        <input 
+          type="radio" 
+          name="option-btn" 
+          value="dog" 
+          checked={selectedOption === 'dog'}
+          onChange={handleChange}/>
+        <label>Dog</label><br/>
+      </form>
+
+      <div className="results-container">
+        { selectedOption === 'cat' &&
+            <img className="cat" src={catImg} />
+        }
+        { selectedOption === 'dog' &&
+            <img className="dog" src={dogImg} />
+        }
+      </div>
     </div>
   );
 }
